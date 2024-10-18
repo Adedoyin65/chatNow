b80771fe2a4a6ec645ed5b5b5e3617d31d609b3d
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-10-18 10:01:05 +0100

    Normalize line endings

---
+++ server.js
@@ -0,0 +1,93 @@
+// Import the express micro-framework
+const express = require("express");
+
+// Import the built-in http module
+const http = require("http");
+
+// Import the Server class from the socket.io library
+const { Server } = require("socket.io");
+
+// Import the path module to work with file paths
+const path = require("path");
+
+//Import the body-parser module
+const bodyParser = require("body-parser");
+
+// Create an instance of the express framework
+const app = express();
+
+// Create a server using the built-in http module binding app
+const server = http.createServer(app);
+
+// Create an instance/object of the Server class
+const io = new Server(server);
+
+// Serve static files (e.g., CSS, JS)
+app.use(express.static(__dirname + "/public"));
+
+//To parse incoming form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const handlebars = require("express3-handlebars").create({
+  defaultLayout: "main",
+});
+
+app.engine("handlebars", handlebars.engine);
+
+app.set("view engine", "handlebars");
+
+// Define the route to serve the index.html file
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup", { title: "Signup" });
+});
+
+app.get("/userpage", (req, res) => {
+  res.render("user", { title: "User" });
+});
+
+app.post("/signup", function (req, res) {
+  const { username, password } = req.body;
+  console.log(req.body);
+  console.log(`Username: ${username}, Password: ${password}`);
+
+  res.json({ redirect: "/login" });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login", { title: "Login" });
+});
+
+// Adding an event listener to the io object
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle text message
+  socket.on("chat message", (msg) => {
+    console.log(`Message received: ${msg}`);
+    io.emit("chat message", msg); // Broadcast the message to all clients
+  });
+
+  // Handle file message
+  socket.on("file message", (fileData) => {
+    console.log(`File received: ${fileData.fileName}`);
+    io.emit("file message", fileData); // Broadcast the file to all clients
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Define the port
+const port = process.env.PORT || 3000;
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log(`Server is running on: ${port}`);
+});
+
